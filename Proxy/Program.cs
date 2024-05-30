5d94e03d7c390208af1af9f088d6b45fa7221363
Kashif Makhdoom <kashifmakhdoom@gmail.com>
2024-05-30 23:22:41 +0500

    Implemented Proxy Pattern

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,1 +1,31 @@
 ﻿Console.Title = "Proxy";
+
+// without Proxy
+Console.WriteLine("Constructing document.");
+var myDocument = new Proxy.Document("MyDocument.pdf");
+Console.WriteLine("Document constructed.");
+myDocument.DisplayDocument();
+
+Console.WriteLine();
+
+// with Proxy
+Console.WriteLine("Constructing document proxy.");
+var myDocumentProxy = new Proxy.DocumentProxy("MyDocument.pdf");
+Console.WriteLine("Document proxy constructed.");
+myDocumentProxy.DisplayDocument();
+
+// with Chained Proxy
+Console.WriteLine("Constructing protected document proxy.");
+var myProtectedDocumentProxy = new Proxy.ProtectedDocumentProxy("MyDocument.pdf", "Viewer");
+Console.WriteLine("Protected document proxy constructed.");
+myProtectedDocumentProxy.DisplayDocument();
+
+Console.WriteLine();
+
+// with Chained Proxy, no access
+Console.WriteLine("Constructing protected document proxy.");
+myProtectedDocumentProxy = new Proxy.ProtectedDocumentProxy("MyDocument.pdf", "Anonymous");
+Console.WriteLine("Protected document proxy constructed.");
+myProtectedDocumentProxy.DisplayDocument();
+
+Console.ReadKey();
---
+++ Proxy/ReadME.txt
@@ -0,0 +1,27 @@
+﻿# INTENT
+
+The intent of this pattern is to provide a surrogate or placeholder for another object to 
+	control access to it
+
+* Subject: defines the common interface between the RealSubject and the Proxy.
+* RealSubject: defines the real object that the proxy presents.
+* Proxy: provides an interface identical to the Subject. It maintains a reference
+	to and controls access to the RealSubject.
+
+
+# USE-CASES
+
+* Use the pattern when you need to add some form of access control to an actual object
+
+# CONSEQUENCES
+
++ It allows introducing new proxies without changing the client code: OCP
+- Added complexity because of additional classes
+- Performance impact of passing through additional layers
+
+
+
+
+
+
+
